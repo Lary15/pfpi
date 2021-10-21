3fd639a2fd927c858d930be283e6c4967e8a7a52
JoaoVictorCabraldeMelo <joao.victor.cabralmelo@gmail.com>
2021-10-21 16:34:53 -0300

    Add: Routes

--- comments/src/main.rs
+++ comments/src/main.rs
@@ -1,58 +1,92 @@
 #[macro_use] extern crate rocket;
-// WIP
-// use rocket::request::Request;
-// use rocket::data::{self,Data, FromData};
-// use rocket::http::{Status, ContentType};
 
-// struct CommentAnswer {
-//     user_id: String,
-//     answer_id: String,
-// }
+use uuid::Uuid;
+use rocket::serde::{Deserialize, Serialize, json::Json};
 
-// #[rocket::async_trait]
-// impl FromData for CommentAnswer {
-//     type Error = String;
-
-//     async fn from_data(req: &'r Request<'_>, data: Data<'r>) -> data::Outcome<'r, Self>{
-//         use Error::*;
-//         use rocket::outcome::Outcome;
-
-
-//         let comment_ct = ContentType::new("application", "x-C")
-//     }
-
-// }
-
-
-
-#[post("/create_answer")]
-fn create_comment_answer() -> String {
-    let x : String = String::from("Criei Comentário para resposta");
-    return x;
+#[derive(Deserialize)]
+struct Comment <'r> {
+    message: & 'r str,
 }
 
-#[post("/create_question")]
-fn create_comment_question() -> String {
-    let x : String = String::from("Criei Comentário para pergunta");
-    return x;
+#[derive(Deserialize)]
+struct EditComment <'r> {
+    id: Uuid,
+    message: &'r str,
 }
 
-#[put("/edit")]
-fn edit_comment() -> String {
-    let x : String = String::from("Editei Comentário");
-    return x;
+#[derive(Deserialize)]
+struct GetAllComments {
+    id: Uuid,
 }
 
-#[delete("/delete")]
-fn delete_comment() -> String {
-    let x : String = String::from("Deletei Comentário");
-    return x;
+#[derive(Serialize)]
+struct ResponseComment <'r> {
+    user_id: Uuid,
+    qa_id: Uuid,
+    id: Uuid,
+    comment: & 'r str,
 }
 
-#[get("/all")]
-fn get_all_comment() -> String {
-    let x : String = String::from("Peguei todos os comentários de uma Q&A");
-    return x;
+#[derive(Serialize)]
+struct EditResponse <'r> {
+    message: &'r str,
+}
+
+#[derive(Serialize)]
+struct DeleteResponse {
+    message: & 'static str,
+}
+
+#[derive(Serialize)]
+struct AllComment {
+    id: Uuid,
+    message: & 'static str,
+}
+
+#[derive(Serialize)]
+struct GetAllResponse  {
+    comments: Vec<AllComment>, 
+}
+
+
+
+
+#[post("/create_answer?<user_id>&<answer_id>", data = "<comment>")]
+fn create_comment_answer(user_id: Uuid, answer_id: Uuid, comment: Json<Comment<'_>>) -> Json<ResponseComment> {
+
+    let comment_id = Uuid::new_v4();
+
+    return Json(ResponseComment { user_id: user_id, qa_id: answer_id, comment: comment.message, id: comment_id })
+}
+
+#[post("/create_question?<user_id>&<question_id>", data = "<comment>")]
+fn create_comment_question(user_id: Uuid, question_id: Uuid, comment: Json<Comment<'_>> ) -> Json<ResponseComment> {
+
+    let comment_id = Uuid::new_v4();
+
+    return Json(ResponseComment { user_id: user_id, qa_id: question_id, comment: comment.message, id: comment_id })
+}
+
+#[put("/edit",data = "<comment>")]
+fn edit_comment(comment: Json<EditComment<'_>>) -> Json<EditResponse> {
+    return Json(EditResponse { message: "Comentário Editado com sucesso"});
+}
+
+#[delete("/delete?<comment_id>")]
+fn delete_comment(comment_id: Uuid) -> Json<DeleteResponse> {
+    return Json(DeleteResponse { message: "Comentário Deletado com sucesso" });
+}
+
+#[get("/all", data = "<qa>")]
+fn get_all_comment(qa: Json<GetAllComments>) -> Json<GetAllResponse> {
+
+    let comment_first = Uuid::new_v4();
+
+    let comment_second  = Uuid::new_v4();
+
+    let comments = vec![AllComment{ id: comment_first, message: "oi lindaaa sz" }, AllComment{ id: comment_second, message: "como você tá ?" }];
+
+    return Json(GetAllResponse { comments: comments });
 }
 
 
